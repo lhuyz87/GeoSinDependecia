e9e528bcbb17a8421ca9e5d84a1caa104c0f9d06
lhuyz87 <lhuyz87@gmail.com>
2020-05-09 20:43:04 -0500

    Update XpathPagPrincipalCentProc.java

--- src/test/java/rimac/geo/xpath/XpathPagPrincipalCentProc.java
+++ src/test/java/rimac/geo/xpath/XpathPagPrincipalCentProc.java
@@ -31,11 +31,12 @@ public class XpathPagPrincipalCentProc {
 	public final String msjRegistroExito="//div[contains(text(),'Registro')]";
 	public final String msjRegistroPlacaExis="//div[contains(text(),'Registrado')]";
 	
-	
+	String aux="wrapper";
 	public final String txtPlaca="//input[@name='form_search']";
 	public final String lblEstadoFiltrado="//td[text()='En camino']";
 	public final String btnBuscar="//input[@name='form_submit']";
-	public final String btnDerivarFiltrado="//div[@class='c-table visible-md-block visible-lg-block tb-procurador' ]//*[@type='button'and @title='Seleccionar Procurador' ]";
+//	public final String btnDerivarFiltrado="//div[@class='c-table visible-md-block visible-lg-block tb-procurador' ]//*[@type='button'and @title='Seleccionar Procurador' ]";
+	public final String btnDerivarFiltrado="//*[@id="+aux+"]/section/div[2]/table/tbody/tr/td[9]/button";
 	public final String lstSelAgrupador="//select[@name='form_caso_proveedor']";
 	public final String btnAsignar ="//input[@value='Asignar']";
 }
