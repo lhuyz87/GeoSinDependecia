9c94da69d18790a58cc8fd0519143ddad7d9fad3
lhuyz87 <lhuyz87@gmail.com>
2020-05-09 21:14:17 -0500

    Update XpathPagPrincipalCentProc.java

--- src/test/java/rimac/geo/xpath/XpathPagPrincipalCentProc.java
+++ src/test/java/rimac/geo/xpath/XpathPagPrincipalCentProc.java
@@ -37,7 +37,9 @@ public class XpathPagPrincipalCentProc {
 	public final String btnBuscar="//input[@name='form_submit']";
 //	public final String btnDerivarFiltrado="//div[@class='c-table visible-md-block visible-lg-block tb-procurador' ]//*[@type='button'and @title='Seleccionar Procurador' ]";
 //	public final String btnDerivarFiltrado="//*[@id="+aux+"]/section/div[2]/table/tbody/tr/td[9]/button";
-	public final String btnDerivarFiltrado="//td[text()='Solicitado']//following::td[1]//*[@title='Seleccionar Procurador']";
+//	public final String btnDerivarFiltrado="//td[text()='Solicitado']//following::td[1]//*[@title='Seleccionar Procurador']";
+	public final String btnDerivarFiltrado="//*[@title='Seleccionar Procurador']";
+	
 	public final String lstSelAgrupador="//select[@name='form_caso_proveedor']";
 	public final String btnAsignar ="//input[@value='Asignar']";
 }
